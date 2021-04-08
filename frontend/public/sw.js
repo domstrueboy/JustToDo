3717b1928c80b40b9ab1f9ca689c0238d2504865
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-04-08 20:37:33 +0500

    added PWA drafts

---
+++ frontend/public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('sw-cache').then(cache => cache.addAll([
+      '/JustToDo/',
+      '/JustToDo/css/style.css',
+      '/JustToDo/js/main.js',
+      '/JustToDo/index.html',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request)),
+  );
+});
