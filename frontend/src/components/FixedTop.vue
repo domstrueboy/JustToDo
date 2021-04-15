b6e04feba5de0f41763f4d1fcb8731458075e15c
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-04-15 18:25:57 +0500

    wip

--- frontend/src/components/FixedTop.vue
+++ frontend/src/components/FixedTop.vue
@@ -4,6 +4,14 @@
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'FixedTop',
+})
+</script>
+
 <style scoped>
   div {
     position: fixed;
