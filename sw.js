d193332d541088e7e13ff422aa6ecc59403a671a
domstrueboy <domstrueboy@yandex.ru>
2021-05-23 19:36:36 +0000

    Deploying to gh-pages from @ domstrueboy/JustToDo@32c3028aad726dceddb07112c420095192b3f7d9 🚀

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('sw-cache').then((cache) => cache.addAll([
-      '/JustToDo/',
-      '/JustToDo/index.html',
-      '/JustToDo/assets/',
-      // '/JustToDo/assets/index.*.css',
-      // '/JustToDo/assets/index.*.js',
-      // '/JustToDo/assets/vendor.*.js',
-    ])),
-  );
-});
+console.log('Hey from SW!');
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open('sw-cache').then((cache) => cache.addAll([
+//       '/JustToDo/',
+//       '/JustToDo/index.html',
+//       '/JustToDo/assets/',
+//       '/JustToDo/assets/index.*.css',
+//       '/JustToDo/assets/index.*.js',
+//       '/JustToDo/assets/vendor.*.js',
+//     ])),
+//   );
+// });
 
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request)),
-  );
-});
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => response || fetch(event.request)),
+//   );
+// });
