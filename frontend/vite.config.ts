32c3028aad726dceddb07112c420095192b3f7d9
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-05-24 00:35:46 +0500

    removed pwa plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Just TODO',
-        short_name: 'TODO',
-        start_url: 'https://domstrueboy.github.io/JustToDO/',
-        display: 'standalone',
-        theme_color: '#ffffff',
-        background_color:'#ffffff',
-        icons: [
-          {
-            src: '/JustToDo/simple-to-do-logo-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ],
-      },
-    }),
   ],
 });
